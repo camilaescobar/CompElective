651b93f3dd820c561b9714f31070d0350b4eec85
camilaescobar <17cescobar@iaa.edu.ec>
2015-05-08 08:22:07 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,10 @@
+operand1 = 95
+operand2 = 64.5 
+
+print "95 + 64.5 =",operand1 + operand2
+print "95 - 64.5 =",operand1 - operand2
+print "95 * 64.5=",operand1 * operand2 
+print "95 / 64.5 =",operand1 / operand2
+print "95 % 64.5 =", operand1 % operand2
+
+#commment my program creates efficient mathematical solutions for variables created. 
