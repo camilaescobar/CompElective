e56bd0e625af3a957b9dba0d768d7490adefb671
camilaescobar <17cescobar@iaa.edu.ec>
2015-05-08 08:19:37 -0500

    Create HelloWorld .py

---
+++ HelloWorld .py
@@ -0,0 +1,12 @@
+print "Hello World"
+print "My name is Camila. \nToday is Wednesday"
+print 2015 #printing an interger
+print "2015" #printing a string
+print 3.145 #printing a floating point number
+
+print 2 + 2 
+print 2-2
+print 2*2
+print 2/2 
+print 3 % 2
+print 3.1416 * 2.8
