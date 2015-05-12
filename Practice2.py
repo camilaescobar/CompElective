6be2bbb208d9b1ee9b9834cb0d659e3f899f5045
camilaescobar <17cescobar@iaa.edu.ec>
2015-05-12 08:59:55 -0500

    Create Practice2.py

---
+++ Practice2.py
@@ -0,0 +1,8 @@
+import sys 
+
+#print "you are using command line arguments", sys.argv
+#print "The first argument you entered is:", sys.argv[1]
+#print "The second argument you entered is:", sys.argv [2]
+#print "the third argument you entered is:", sys.argv [3]
+
+print "my name is", sys.argv[1]
