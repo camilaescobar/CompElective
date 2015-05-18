84a4d33fdaee3a1beb589ed7eae910cb8ca35fca
camilaescobar <17cescobar@iaa.edu.ec>
2015-05-18 08:10:46 -0500

    Create Practice3.py

---
+++ Practice3.py
@@ -0,0 +1,22 @@
+#create a form with the following info
+#name
+#lastname
+#year of birth
+#favorite singer/band 
+#favorite movie 
+#best friend
+#calculate age in 10 years
+#print name 10 times 
+
+name = raw_input("What is your name?")
+lastname = raw_input("What is your last name?")
+yob = input("What year were you born in?")
+favband = raw_input("What is your favorite band?")
+favmov= raw_input ("What's your favorite movie?")
+bff= raw_input ("Who is your best friend?") 
+age= input("How old are you?")
+
+print name, "you are", age, "years old"
+print name, "in 10 years you will be", age+10, "years old"
+
+print "Camila Camila Camila Camila Camila Camila Camila Camila Camila Camila" 
